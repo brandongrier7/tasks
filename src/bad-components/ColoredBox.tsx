29591d7dfac91ef947828cbae7173142e3bd3015
Brandon Grier <bgrier20@20mbpro379.myfiosgateway.com>
2022-03-29 14:46:12 -0400

    solved components

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,23 +4,23 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+interface changeColor {
+    changeColor: () => void;
 }
-
-function ColorPreview(): JSX.Element {
+interface preview {
+    lookAtColor: string;
+}
+function ChangeColor({ changeColor }: changeColor): JSX.Element {
+    return <Button onClick={changeColor}>Next Color</Button>;
+}
+function ColorPreview({ lookAtColor }: preview): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: lookAtColor,
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +30,15 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+    const switchColor = () => setColorIndex((colorIndex + 1) % COLORS.length);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]} </span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor changeColor={switchColor}></ChangeColor>
+                <ColorPreview lookAtColor={COLORS[colorIndex]}></ColorPreview>
             </div>
         </div>
     );
